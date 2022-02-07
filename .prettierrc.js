e79b16f51ee04b6c8e85256fc849eca1f1a448ba
Nicolas Payot <nicolas.payot@gmail.com>
2022-02-07 12:47:48 +0000

    Add some dependencies

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    quoteProps: 'consistent',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+};
